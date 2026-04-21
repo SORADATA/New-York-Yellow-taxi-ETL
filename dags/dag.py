61fd11eb5608998c85c5771a12692b1358609c4f
MoussaTheAnalyst <163172464+SORADATA@users.noreply.github.com>
2026-04-21 15:49:39 +0200

    refactor: Add comments for Airflow 3.2 and project variables
    Added comments for Airflow 3.2 compatibility and project variables.

--- dags/dag.py
+++ dags/dag.py
@@ -1,9 +1,13 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.models.baseoperator import chain
+#------------------- Avec Airflow 3.2---------------------------------#
+# from airflow.sdk import dag
+# from airflow.sdk.base.operator import chain
 
 from tasks import Extract, Load
-
+# Variables 
+# nom_projet = "NYW-ETL"
 default_args = {
     'owner': 'Moussa SISSOKO',
     'depends_on_past': False,
